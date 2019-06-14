928e8a90f7efa06080a6eb87e02a7708e03ae6b7
Gregory McWhirter <greg@ideafreemonoid.org>
2019-06-13 17:43:07 -0700

    Add multiple quote support to the tokenizer

--- parser/tokenizer.go
+++ parser/tokenizer.go
@@ -9,9 +9,19 @@ import (
 // ErrTokenizeError represents an error tokenizing where there was an opening quote without a paired closing
 var ErrTokenizeError = errors.New("error tokenizing")
 
+func isQuote(char rune, quots []rune) bool {
+	for _, quot := range quots {
+		if char == quot {
+			return true
+		}
+	}
+
+	return false
+}
+
 // Tokenize will split a string into delim-separated tokens, accounting for quoted
 // sections and escapes
-func Tokenize(msg string, delim rune, escape rune, quot rune) ([]string, error) {
+func Tokenize(msg string, delim rune, escape rune, quots []rune) ([]string, error) {
 	msgR := []rune(msg)
 	max := strings.Count(msg, string(delim)) + 1
 	tokens := make([]string, 0, max)
@@ -24,12 +34,12 @@ func Tokenize(msg string, delim rune, escape rune, quot rune) ([]string, error) 
 		r = msgR[i]
 
 		if inQuote {
-			if r == quot {
+			if isQuote(r, quots) {
 				inQuote = false
 				continue
 			}
 
-			if r == escape && i < len(msgR)-1 && msgR[i+1] == quot {
+			if r == escape && i < len(msgR)-1 && isQuote(msgR[i+1], quots) {
 				buffer = append(buffer, msgR[i+1])
 				i++ // skip over next, having added it already
 				continue
@@ -46,12 +56,12 @@ func Tokenize(msg string, delim rune, escape rune, quot rune) ([]string, error) 
 			continue
 		}
 
-		if r == quot {
+		if isQuote(r, quots) {
 			inQuote = true
 			continue
 		}
 
-		if r == escape && i < len(msgR)-1 && (msgR[i+1] == delim || msgR[i+1] == quot) {
+		if r == escape && i < len(msgR)-1 && (msgR[i+1] == delim || isQuote(msgR[i+1], quots)) {
 			buffer = append(buffer, msgR[i+1])
 			i++ // skip over next, having added it already
 			continue
