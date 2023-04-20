4bdbb7458fac0fea0dce180aba2008ab50386cd9
Gregory McWhirter <greg@ideafreemonoid.org>
2023-04-20 11:06:32 -0400

    Add Number to re-exported types

--- json/wrapping.go
+++ json/wrapping.go
@@ -2,4 +2,7 @@ package json
 
 import "encoding/json" //nolint:depguard // we need this for aliasing
 
-type RawMessage = json.RawMessage
+type (
+	RawMessage = json.RawMessage
+	Number     = json.Number
+)
