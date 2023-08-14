9dc706e259ffc9ab6797b4bb4b330c95b5ef8bc0
Gregory McWhirter <greg@ideafreemonoid.org>
2023-08-14 19:48:15 -0400

    Add handler wrapper

--- telemetry/keys.go
+++ telemetry/keys.go
@@ -1,6 +1,9 @@
 package telemetry
 
 import (
+	"net/http"
+
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"go.opentelemetry.io/otel/semconv/v1.20.0/httpconv"
 )
 
@@ -14,3 +17,7 @@ var (
 	HTTPAttributesFromHTTPStatusCode    = httpconv.ClientResponse
 	SpanStatusFromHTTPStatusCode        = httpconv.ClientStatus
 )
+
+func Handler(h http.Handler, name string, t *Telemeter) http.Handler {
+	return otelhttp.NewHandler(h, name, otelhttp.WithTracerProvider(t), otelhttp.WithMeterProvider(t))
+}
